4eb8e35669a9b8066fa857ff3e5ef43b633b702d
yuezhiyuanting <104759068+yuezhiyuanting@users.noreply.github.com>
2023-03-09 21:35:54 +0800

    Update GBDT_data_analysis_1.py

--- GBDT_data_analysis_1.py
+++ GBDT_data_analysis_1.py
@@ -1,59 +1,40 @@
 import streamlit as st
 from sklearn import metrics
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor  # 导入回归模型
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-# import mysql
 import pymysql
 
-
-# @st.cache_resource
-# def init_connection():
-#     return mysql.connector.connect(**st.secrets["mysql"])
-#
-# conn = init_connection()
-#
-# @st.cache_data(ttl=600)
-# def run_query(query):
-#     with conn.cursor() as cur:
-#         cur.execute(query)
-#         return cur.fetchall()
-#
-# rows = run_query("select `HPC+HPT组合件相位（°）` from `1`;"
-#                  "select  `HPC+HPT组合件初始不平衡大小（gmm）` from `1`;"
-#                  "select  `HPC+HPT组合件初始不平衡角度（°）` from `1`;"
-#                  "select  `HPC+HPT组合件同心度（mm）` from `1`")
-# data=pd.DataFrame(rows)
-
 # 导入数据
-@st.cache_data  # 快速缓存
-def load_data(sql):
-    mydb = pymysql.connect(
-        host='192.168.43.131',
-        user='root',
-        database='hebing_1',
-        passwd='asd25380'
-    )
-    cursor = mydb.cursor(pymysql.cursors.DictCursor)
-    data_1 = []
-    for i in sql:
-        cursor.execute(i)
-        result = cursor.fetchall()
-        data_1.append(pd.DataFrame(result))
-    data = data_1[0]
-    for i in range(1, len(data_1)):
-        data = data.join([data_1[i]], how='outer')
-    return data
+# @st.cache_data  # 快速缓存
+# def load_data(sql):
+#     mydb = pymysql.connect(
+#         host='localhost',
+#         user='root',
+#         database='hebing_1',
+#         passwd='asd25380'
+#     )
+#     cursor = mydb.cursor(pymysql.cursors.DictCursor)
+#     data_1 = []
+#     for i in sql:
+#         cursor.execute(i)
+#         result = cursor.fetchall()
+#         data_1.append(pd.DataFrame(result))
+#     data = data_1[0]
+#     for i in range(1, len(data_1)):
+#         data = data.join([data_1[i]], how='outer')
+#     return data
+# 
+# 
+# sql = ['select `HPC+HPT组合件相位（°）` from `1`',
+#        'select  `HPC+HPT组合件初始不平衡大小（gmm）` from `1`',
+#        'select  `HPC+HPT组合件初始不平衡角度（°）` from `1`',
+#        'select  `HPC+HPT组合件同心度（mm）` from `1`']
+# data = load_data(sql)
 
-
-sql = ['select `HPC+HPT组合件相位（°）` from `1`',
-       'select  `HPC+HPT组合件初始不平衡大小（gmm）` from `1`',
-       'select  `HPC+HPT组合件初始不平衡角度（°）` from `1`',
-       'select  `HPC+HPT组合件同心度（mm）` from `1`']
-data = load_data(sql)
-
+data=pd.read_excel('第8组数据.xlsx')
 
 # 设置网页标题
 st.title('基于streamlit的可视化界面')
@@ -102,5 +83,5 @@ if genre in col:
     plt.grid(True)
     st.pyplot(fig)
 
-# GBDT_data_analysis_1.py
+# GBDT_data_analysis_2.py
 
